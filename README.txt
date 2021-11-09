837f86b35534158466e4304097c44cee67d52b5e
Luke Horwell <code@horwell.me>
2021-11-09 15:33:00 +0000

    Initial release

---
+++ README.txt
@@ -0,0 +1,3 @@
+
+Place a "CustomCursor.png" image into the root of the "Cities_Skylines" folder.
+The cursor will change once the level has loaded.
---
+++ Source/CustomCursor.cs
@@ -0,0 +1,49 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace CustomCursor
+{
+    public class CustomCursors : IUserMod
+    {
+        public string Name
+        {
+            get { return "Custom Cursor"; }
+        }
+
+        public string Description
+        {
+            get { return "Use CustomPointer.png for the cursor pointer"; }
+        }
+
+        public class CustomMouseEvent : MonoBehaviour {
+            public Texture2D cursorTexture = new Texture2D(48, 48);
+            public CursorMode cursorMode = CursorMode.Auto;
+            public Vector2 hotSpot = Vector2.zero;
+
+            void Start()
+            {
+                string filename = "CustomPointer.png";
+                var rawData = System.IO.File.ReadAllBytes(filename);
+                cursorTexture.LoadImage(rawData);
+                Update();
+            }
+
+            void Update()
+            {
+                UnityEngine.Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+            }
+        }
+        
+        public class Loader : LoadingExtensionBase
+        {
+            public override void OnLevelLoaded(LoadMode mode)
+            {
+                // Instantiate a custom object
+                GameObject go = new GameObject("Custom Cursor Object");
+                go.AddComponent<CustomMouseEvent>();
+
+                base.OnLevelLoaded(mode);
+            }
+        }
+    }
+}
